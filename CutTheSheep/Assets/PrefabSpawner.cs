d20f6f0a5c248464b91774c5ab743900994da5dc
Dan-Mechanics <github.tim@desmidt.org>
2024-10-16 19:25:37 +0200

    yeet

---
+++ CutTheSheep/Assets/PrefabSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CutTheSheep
+{
+    public class PrefabSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab = null;
+        [SerializeField] private Transform spawnExample = null;
+        [SerializeField] [Min(0.1f)] private float interval = 0f;
+
+        private void Start()
+        {
+            InvokeRepeating(nameof(Spawn), 0f, interval);
+        }
+
+        private void Spawn() 
+        {
+            GameObject newlySpawned = Instantiate(prefab, spawnExample.position, spawnExample.rotation);
+            newlySpawned.name = prefab.name;
+        }
+    }
+}
