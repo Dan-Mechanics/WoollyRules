6f3246153aa2a8e7864aefdbfb89dbd502dd4ba0
Dan-Mechanics <github.tim@desmidt.org>
2024-10-21 11:56:36 +0200

    planet lore

---
+++ CutTheSheep/Assets/PlanetGravity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CutTheSheep
+{
+    public class PlanetGravity : MonoBehaviour
+    {
+        private Rigidbody rb;
+        private Transform planet;
+
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+
+            rb.useGravity = false;
+            planet = GameObject.FindWithTag("Planet").transform;
+        }
+
+        private void FixedUpdate()
+        {
+            rb.AddForce((planet.position - transform.position).normalized * Physics.gravity.magnitude, ForceMode.Acceleration);
+        }
+    }
+}
