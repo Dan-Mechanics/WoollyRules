e04774d8be158812f91fbc4e609e9f03c52db677
Dan-Mechanics <github.tim@desmidt.org>
2024-10-24 16:54:22 +0200

    added new project

---
+++ CutTheSheepTwo/Assets/Sophie/Scripts/InteractiveGrass.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class InteractiveGrass : MonoBehaviour
+{
+    public Material planetMaterial; // Assign the material
+    public GameObject[] entities;   // Array of entities (limit to 4 for performance)
+
+    void Update()
+    {
+        // Send entity positions to the shader (limiting to 4 positions here)
+        for (int i = 0; i < Mathf.Min(entities.Length, 4); i++)
+        {
+            Vector3 entityPosition = entities[i].transform.position;
+            planetMaterial.SetVector($"_EntityPosition{i + 1}", new Vector4(entityPosition.x, entityPosition.y, entityPosition.z, 1.0f)); // Pass positions
+        }
+    }
+}
