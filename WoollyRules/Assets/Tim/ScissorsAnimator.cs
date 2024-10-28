649c502f0710137de3c9e8f2e29e4c2d646a3c7e
Dan-Mechanics <github.tim@desmidt.org>
2024-10-28 13:05:49 +0100

    yeet

---
+++ WoollyRules/Assets/Tim/ScissorsAnimator.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+namespace WoollyRules
+{
+    public class ScissorsAnimator : MonoBehaviour
+    {
+        [SerializeField] private Transform scissors = null;
+
+        [SerializeField] private Transform scissorsForward = null;
+        [SerializeField] private Transform scissorsBackward = null;
+
+        [SerializeField] private Transform[] scissorParts = null;
+
+        [SerializeField] private Transform[] scissorPartsOpen = null;
+        [SerializeField] private Transform[] scissorPartsClosed = null;
+
+        [SerializeField] private float forwardScissorsForwardAmount = 0f;
+
+        [SerializeField] private float cutLerpValue = 0f;
+        [SerializeField] private float hoverLerpValue = 0f;
+
+        public bool open;
+        public bool forward;
+
+        private void Start()
+        {
+            if (scissorParts.Length != scissorPartsOpen.Length || scissorParts.Length != scissorPartsClosed.Length) 
+            {
+                Debug.LogError("if (scissorParts.Length != scissorPartsOpen.Length || scissorParts.Length != scissorPartsClosed.Length) !!");
+                Destroy(gameObject);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            /*open = Input.GetKey(KeyCode.Space);
+            forward = Input.GetKey(KeyCode.W);*/
+
+            scissorsForward.forward = scissors.forward;
+            scissorsForward.localPosition = scissorsBackward.localPosition + scissorsForward.forward * forwardScissorsForwardAmount;
+
+            scissors.localPosition = Vector3.Lerp(scissors.localPosition, forward ? scissorsForward.localPosition : scissorsBackward.localPosition, cutLerpValue);
+            
+            for (int i = 0; i < scissorParts.Length; i++)
+            {
+                scissorParts[i].localRotation = Quaternion.Lerp(scissorParts[i].localRotation, open ? scissorPartsOpen[i].localRotation : scissorPartsClosed[i].localRotation, hoverLerpValue);
+            }
+        }
+
+    }
+}
