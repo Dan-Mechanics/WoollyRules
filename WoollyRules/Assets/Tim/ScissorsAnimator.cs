8285f7b5fa06df9277262a34a8c6f9e56f7af877
Dan-Mechanics <github.tim@desmidt.org>
2024-10-28 16:15:20 +0100

    works

--- WoollyRules/Assets/Tim/ScissorsAnimator.cs
+++ WoollyRules/Assets/Tim/ScissorsAnimator.cs
@@ -7,19 +7,19 @@ namespace WoollyRules
         [SerializeField] private Transform scissors = null;
 
         [SerializeField] private Transform scissorsForward = null;
-        [SerializeField] private Transform scissorsBackward = null;
+      //  [SerializeField] private Transform scissorsBackward = null;
 
         [SerializeField] private Transform[] scissorParts = null;
 
         [SerializeField] private Transform[] scissorPartsOpen = null;
         [SerializeField] private Transform[] scissorPartsClosed = null;
 
-        [SerializeField] private float forwardScissorsForwardAmount = 0f;
+       // [SerializeField] private float forwardScissorsForwardAmount = 0f;
 
         [SerializeField] private float cutLerpValue = 0f;
         [SerializeField] private float hoverLerpValue = 0f;
 
-        [SerializeField] private Transform forward_ = null;
+        //[SerializeField] private Transform forward_ = null;
 
         public bool open;
         public bool forward;
@@ -38,10 +38,7 @@ namespace WoollyRules
             open = Input.GetKey(KeyCode.Mouse1);
             forward = Input.GetKey(KeyCode.Mouse0);
 
-            scissorsForward.forward = scissors.forward;
-            scissorsForward.localPosition = scissorsBackward.localPosition + scissorsForward.forward * forwardScissorsForwardAmount;
-
-            scissors.localPosition = Vector3.Lerp(scissors.localPosition, forward ? scissorsForward.localPosition : scissorsBackward.localPosition, cutLerpValue);
+            scissors.localPosition = Vector3.Lerp(scissors.localPosition, forward ? scissorsForward.localPosition : Vector3.zero, cutLerpValue);
             
             for (int i = 0; i < scissorParts.Length; i++)
             {
