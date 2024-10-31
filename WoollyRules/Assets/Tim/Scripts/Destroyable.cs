0ce0fb5c24bf126efbf3fc2b94c019aaecbc8e86
Dan-Mechanics <github.tim@desmidt.org>
2024-10-31 19:10:00 +0100

    timertext

--- WoollyRules/Assets/Tim/Scripts/Destroyable.cs
+++ WoollyRules/Assets/Tim/Scripts/Destroyable.cs
@@ -5,6 +5,8 @@ namespace WoollyRules
     /// <summary>
     /// should make it so we have bool here with insta destroy or time perhaps ??
     /// or maybe just remove the idea of destroing from start in the first place like damn
+    /// 
+    /// disabler.
     /// </summary>
     public class Destroyable : MonoBehaviour
     {
--- WoollyRules/Assets/Tim/Scripts/TimerText.cs
+++ WoollyRules/Assets/Tim/Scripts/TimerText.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 
 namespace WoollyRules
 {
+    /// <summary>
+    /// 05:50 time meme.
+    /// </summary>
     public class TimerText : MonoBehaviour
     {
         [SerializeField] private Text text = null;
