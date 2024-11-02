5e79e357a0974da58962b1d3e50f4e37ccabeffd
Dan-Mechanics <github.tim@desmidt.org>
2024-11-02 10:45:21 +0100

    yeet

--- WoollyRules/Assets/Tim/Scripts/Sheep/SheepMovement.cs
+++ WoollyRules/Assets/Tim/Scripts/Sheep/SheepMovement.cs
@@ -2,44 +2,55 @@ using UnityEngine;
 
 namespace WoollyRules.Sheep
 {
+    /// <summary>
+    /// Link with sheep animation with property
+    /// make the rotation better.
+    /// </summary>
     public class SheepMovement : MonoBehaviour
     {
+        public float FlatSpeed { get; private set; }
+
         [SerializeField] private Rigidbody rb = null;
         [SerializeField] private float forwardAccel = 0f;
         [SerializeField] private float maxSpeed = 0f;
         [SerializeField] private float maxRotationSpeed = 0f;
-        [SerializeField] private float rotationDirectionChangeInterval = 0f;
+        [SerializeField] private float maxChangeDirInterval = 0f;
         [SerializeField] private Transform forceApplyPoint = null;
 
         private float rotationDirection;
+        private Vector3 velocity;
 
         private void Start()
         {
-            InvokeRepeating(nameof(ChangeRotationDirection), 0f, rotationDirectionChangeInterval);
+            Invoke(nameof(ChangeRotationDirection), Random.Range(0f, maxChangeDirInterval));
         }
 
         private void FixedUpdate()
         {
-            Vector3 vel = rb.linearVelocity;
-
-            // idk if this is the right one.
-            vel = transform.InverseTransformDirection(vel);
-            vel.y = 0f;
-
             transform.Rotate(rotationDirection * Time.fixedDeltaTime * Vector3.up, Space.Self);
 
             rb.AddForceAtPosition(transform.forward * forwardAccel, forceApplyPoint.position, ForceMode.Acceleration);
 
-            vel = Vector3.ClampMagnitude(vel, maxSpeed);
-            vel = transform.TransformDirection(vel);
-            vel.y = rb.linearVelocity.y;
+            velocity = rb.linearVelocity;
 
-            rb.linearVelocity = vel;
+            // idk if this is the right one.
+            velocity = transform.InverseTransformDirection(velocity);
+            velocity.y = 0f;
+
+            FlatSpeed = velocity.magnitude;
+
+            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            velocity = transform.TransformDirection(velocity);
+            velocity.y = rb.linearVelocity.y;
+
+            rb.linearVelocity = velocity;
         }
 
         private void ChangeRotationDirection() 
         {
             rotationDirection = Random.Range(-maxRotationSpeed, maxRotationSpeed);
+
+            Invoke(nameof(ChangeRotationDirection), Random.Range(0f, maxChangeDirInterval));
         }
     }
 }
