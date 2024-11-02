15d00269b35146c45a32220142ffeed9c224014d
Dan-Mechanics <github.tim@desmidt.org>
2024-11-02 17:20:10 +0100

    added new rule text

--- WoollyRules/Assets/Tim/Scripts/TextComponents/TextHighlight.cs
+++ WoollyRules/Assets/Tim/Scripts/TextComponents/TextHighlight.cs
@@ -1,29 +1,32 @@
 using UnityEngine;
 using TMPro;
 using WoollyRules.Core;
+using UnityEngine.UI;
 
 namespace WoollyRules.TextComponents
 {
     public class TextHighlight : MonoBehaviour
     {
         [SerializeField] private TMP_Text text = null;
+        [SerializeField] private Image image = null;
+
+        [SerializeField] private Color normalColor = Color.clear;
         [SerializeField] private Color highlightColor = Color.clear;
         [SerializeField] private Scissors scissors = null;
 
-        private Color normalColor;
-
         private void Start()
         {
-            normalColor = text.color;
+            //ChangeColor(false, false);
             
             scissors.OnHoverFeedback += ChangeColor;
 
             ChangeColor(false, false);
         }
-         
+        
         private void ChangeColor(bool isCuttable, bool ruleBrokenOnCut)
         {
-            text.color = ruleBrokenOnCut ? highlightColor : normalColor;
+            if (text != null) { text.color = ruleBrokenOnCut ? highlightColor : normalColor; }
+            if (image != null) { image.color = ruleBrokenOnCut ? highlightColor : normalColor; }
         }
     }
 }
