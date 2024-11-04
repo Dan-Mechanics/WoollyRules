0a0ef3a1538cd386996b9ea444eaccf13c744d8e
Dan-Mechanics <github.tim@desmidt.org>
2024-11-04 15:54:04 +0100

    yeet

--- WoollyRules/Assets/Tim/Scripts/Components/SceneSwitcher.cs
+++ WoollyRules/Assets/Tim/Scripts/Components/SceneSwitcher.cs
@@ -10,6 +10,17 @@ namespace WoollyRules.Components
             Application.targetFrameRate = 300;
         }
 
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+
+                Switch("Menu");
+            }
+        }
+
         public void Switch(string name)
         {
             SceneManager.LoadScene(name);
