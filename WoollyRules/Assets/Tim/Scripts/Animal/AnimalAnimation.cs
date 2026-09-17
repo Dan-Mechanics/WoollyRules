b8932994da6831e6e6a7702d8a10579e309b172e
Dan-Mechanics <github.tim@desmidt.org>
2026-09-17 21:04:48 +0200

    small changes

--- WoollyRules/Assets/Tim/Scripts/Animal/AnimalAnimation.cs
+++ WoollyRules/Assets/Tim/Scripts/Animal/AnimalAnimation.cs
@@ -5,8 +5,8 @@ namespace WoollyRules
     public class AnimalAnimation : MonoBehaviour
     {
         [SerializeField] private Transform graphic = default;
-        [SerializeField] private Transform state1Graphic = default;
-        [SerializeField] private Transform state2Graphic = default;
+        [SerializeField] private Transform upBounce = default;
+        [SerializeField] private Transform downBounce = default;
         [SerializeField] private float period = default;
         [SerializeField] private float waveMult = default;
         private float startingRandomOffset;
@@ -18,9 +18,9 @@ namespace WoollyRules
         {
             float lerpValue = WaveValue(Time.time + startingRandomOffset) * waveMult;
 
-            graphic.localScale = Vector3.Lerp(state1Graphic.localScale, state2Graphic.localScale, lerpValue);
-            graphic.localPosition = Vector3.Lerp(state1Graphic.localPosition, state2Graphic.localPosition, lerpValue);
-            graphic.localRotation = Quaternion.Lerp(state1Graphic.localRotation, state2Graphic.localRotation, lerpValue);
+            graphic.localScale = Vector3.Lerp(upBounce.localScale, downBounce.localScale, lerpValue);
+            graphic.localPosition = Vector3.Lerp(upBounce.localPosition, downBounce.localPosition, lerpValue);
+            graphic.localRotation = Quaternion.Lerp(upBounce.localRotation, downBounce.localRotation, lerpValue);
         }
 
         private float WaveValue(float x) 
